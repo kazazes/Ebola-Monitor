0a1796e674d487713c82642fef5553cf1028d844
Peter Kazazes <peter@peterk.co>
2016-03-02 12:01:05 -0500

    Add example config, ignore main config

---
+++ server/conf.js.example.js
@@ -0,0 +1,11 @@
+var config = {}
+
+config.mongo = {}
+config.mongo.dbName = 'Zika'
+config.mongo.user = 'zika'
+config.mongo.password = ''
+
+config.TLSPort = 443
+config.PORT = 80
+
+module.exports = config
